f14ff917113138a48a9d5eefa2cfdd3b4d65539c
Frediano Ziglio <frediano.ziglio@cloud.com>
2024-06-29 17:32:31 +0000

    CP-50124: Write tests for some xenstore messages
    Signed-off-by: Frediano Ziglio <frediano.ziglio@cloud.com>

--- libs/xs/xsraw_test.ml
+++ libs/xs/xsraw_test.ml
@@ -12,6 +12,8 @@
  * GNU Lesser General Public License for more details.
  *)
 
+open Thread
+
 let%test_module "valid_path tests" =
   (module struct
     let valid_path path = Xsraw.validate_path path ; true
@@ -66,5 +68,79 @@ let%test_module "split_string tests" =
     let%test _ = split ':' ":a:" [""; "a"; ""]
 
     let%test _ = split ':' "" [""]
+  end
+)
+
+let%test_module "xenstore operation tests" =
+  (module struct
+    let xs_test test_func =
+      let fd0, fd1 =
+        Unix.socketpair ~cloexec:true Unix.PF_UNIX Unix.SOCK_STREAM 0
+      in
+      let con = Xsraw.open_fd fd0 in
+      let res = test_func con fd1 in
+      Xsraw.close con ; Unix.close fd1 ; res
+
+    let xb_packet op data =
+      let pkt = Xenbus.Packet.create 123 0 op data in
+      Xenbus.Packet.to_string pkt
+
+    let reply_data (fd, data) =
+      Unix.write_substring fd data 0 (String.length data) |> ignore ;
+      Unix.shutdown fd Unix.SHUTDOWN_SEND
+
+    let%test "directory with short reply" =
+      xs_test @@ fun con fd ->
+      let data = xb_packet Xenbus.Op.Directory "one\000two\000" in
+      let thread = Thread.create reply_data (fd, data) in
+      let files = Xsraw.directory 123 "/test" con in
+      Thread.join thread ;
+      List.equal ( = ) files ["one"; "two"]
+
+    let%test "directory with long reply, one packet" =
+      xs_test @@ fun con fd ->
+      let data = xb_packet Xenbus.Op.Error "E2BIG\000" in
+      let data = data ^ xb_packet Xenbus.Op.Watchevent "foo\000data" in
+      let data =
+        data ^ xb_packet Xenbus.Op.Directory_part "1\000one\000two\000\000"
+      in
+      let thread = Thread.create reply_data (fd, data) in
+      let files = Xsraw.directory 123 "/test" con in
+      Thread.join thread ;
+      List.equal ( = ) files ["one"; "two"]
+
+    let%test "directory with long reply, two packets" =
+      xs_test @@ fun con fd ->
+      let data = xb_packet Xenbus.Op.Error "E2BIG\000" in
+      let data = data ^ xb_packet Xenbus.Op.Watchevent "foo\000data" in
+      let data = data ^ xb_packet Xenbus.Op.Directory_part "1\000one\000" in
+      let data = data ^ xb_packet Xenbus.Op.Directory_part "1\000two\000\000" in
+      let thread = Thread.create reply_data (fd, data) in
+      let files = Xsraw.directory 123 "/test" con in
+      Thread.join thread ;
+      List.equal ( = ) files ["one"; "two"]
+
+    let%test "directory with long reply, wrong generation" =
+      xs_test @@ fun con fd ->
+      let data = xb_packet Xenbus.Op.Error "E2BIG\000" in
+      let data = data ^ xb_packet Xenbus.Op.Watchevent "foo\000data" in
+      let data = data ^ xb_packet Xenbus.Op.Directory_part "1\000wrong\000" in
+      let data =
+        data ^ xb_packet Xenbus.Op.Directory_part "2\000other\000\000"
+      in
+      let data =
+        data ^ xb_packet Xenbus.Op.Directory_part "2\000this is correct\000\000"
+      in
+      let thread = Thread.create reply_data (fd, data) in
+      let files = Xsraw.directory 123 "/test" con in
+      Thread.join thread ;
+      List.equal ( = ) files ["this is correct"]
+
+    let%test "read" =
+      xs_test @@ fun con fd ->
+      let data = xb_packet Xenbus.Op.Read "value\000" in
+      let thread = Thread.create reply_data (fd, data) in
+      let value = Xsraw.read 123 "/test" con in
+      Thread.join thread ; value = "value"
   end
 )
